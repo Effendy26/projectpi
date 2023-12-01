7268bccaf175cbf70ddf8d5e040886a153444bd2
Effendy26 <effendygabriel2006@gmail.com>
2023-12-01 09:25:04 +0700

    main raket kita by 1

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HALLO PI</title>
+</head>
+<body>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quam molestias excepturi quaerat distinctio cupiditate totam, repellat maxime blanditiis nostrum impedit dolore veniam enim, aut commodi velit officia molestiae corrupti!</P>
+</body>
+</html>
